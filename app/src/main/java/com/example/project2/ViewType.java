41e54193991359eb5c79d8ad11a824c02db16ab7
Lib0823 <robot082373@naver.com>
2022-11-13 03:21:10 +0900

    11.13 Backup(inbeom) Commit

--- app/src/main/java/com/example/project2/ViewType.java
+++
@@ -1,7 +0,0 @@
-package com.example.project2;
-
-public class ViewType{
-    public static final int CENTER_JOIN = 0;
-    public static final int LEFT_CHAT = 1;
-    public static final int RIGHT_CHAT = 2;
-}
